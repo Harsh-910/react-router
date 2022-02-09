b5842fb3d87a327a1f487e05dc28104d30112ae3
Harsh-910 <97285448+Harsh-910@users.noreply.github.com>
2022-02-09 13:23:37 +0530

    initial commit

---
+++ src/pages/welcome.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+
+const welcome = () => {
+  return (
+    <>
+      <h1>welcome Page</h1>
+      {/* <Routes> */}
+      {/* <Route path="/welcome/new_user">
+        <>
+        <h3>Welcome new user</h3>
+        </>
+      </Route>
+      </Routes> */}
+    </>
+  );
+};
+
+export default welcome;
